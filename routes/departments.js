5cc0f76620f4cd8f5e4fcf9b5f4b6fd9b73ddedf
hokagedami <mikedamilare@gmail.com>
2019-05-09 11:19:52 +0100

    First commit:

---
+++ routes/departments.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET all departments*/
+router.get('/', function(req, res, next) {
+
+});
+
+/* GET department by id*/
+router.get('/:id', function(req, res, next) {
+
+});
+
+module.exports = router;
